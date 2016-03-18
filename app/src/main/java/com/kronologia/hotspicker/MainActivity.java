b4f175b9c1103edce12aee8683200f98d766231f
Salkyn <maxence.vialla@gmail.com>
2016-03-18 09:54:32 +0100

    Delete JSONParser and add its code in MainAct (DNF)

--- app/src/main/java/com/kronologia/hotspicker/MainActivity.java
+++ app/src/main/java/com/kronologia/hotspicker/MainActivity.java
@@ -1,13 +1,82 @@
 package com.kronologia.hotspicker;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.kronologia.hotspicker.AppController;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    String url = "kronologia.fr/HotsPicker/Azmodan.json";
+    private String jsonResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        makeJsonArrayRequest();
+    }
+
+    private void makeJsonArrayRequest() {
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+                            jsonResponse = "";
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject person = (JSONObject) response.get(i);
+
+                                String hero1 = person.getString("hero1");
+                                String hero2 = person.getString("hero2");
+                                String winrate = person.getString("winrate");
+
+                                jsonResponse += hero1;
+                                jsonResponse += " vs " + hero2;
+                                jsonResponse += " : " + winrate;
+
+                            }
+
+                            Log.d(TAG, jsonResponse);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
     }
 }
