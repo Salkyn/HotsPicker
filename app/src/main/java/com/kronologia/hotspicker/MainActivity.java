5061fa7d628b9d136343fd18b1ac4b1208ceae00
Salkyn <maxence.vialla@gmail.com>
2016-03-31 09:58:23 +0200

    comment + nettoyage

--- app/src/main/java/com/kronologia/hotspicker/MainActivity.java
+++ app/src/main/java/com/kronologia/hotspicker/MainActivity.java
@@ -6,11 +6,8 @@ import com.kronologia.hotspicker.AppController;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,7 +23,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class MainActivity extends Activity {
 
@@ -44,44 +40,60 @@ public class MainActivity extends Activity {
     LinearLayout imLayout;
 
     String[] heroNames = {"falstad","gall","greymane","illidan","jaina","kaelthas",
-            "kerrigan","liming","lunara","nova","raynor","thebutcher","thrall",
-            "tychus","valla","zeratul","abathur","azmodan","gazlowe","lostvikings",
-            "murky","nazeebo","sgthammer","sylvanas","xul","zagara","brightwing",
-            "kharazim","lili","ltmorales","malfurion","rehgar","tassadar","tyrande",
-            "uther","anubarak","artanis","arthas","chen","cho","diablo","etc","johanna",
-            "leoric","muradin","rexxar","sonya","stitches","tyrael","dehaka"};
+            "kerrigan","lunara","nova","raynor","thebutcher","thrall","tychus",
+            "valla","zeratul","abathur","azmodan","gazlowe","lostvikings","murky",
+            "nazeebo","sgthammer","sylvanas","zagara","brightwing","kharazim",
+            "lili","ltmorales","malfurion","rehgar","tassadar","tyrande","uther",
+            "anubarak","artanis","arthas","chen","cho","diablo","etc","johanna",
+            "leoric","muradin","rexxar","sonya","stitches","tyrael","liming","xul","dehaka"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Arrays.sort(heroNames);
+        Arrays.sort(heroNames); //classement alphabétique des noms pour l'affichage
 
         setContentView(R.layout.activity_main);
 
+        //Layout qui contient les images de tous les héros
         imLayout = (LinearLayout) findViewById(R.id.images_layout);
 
         imAllies1 = (ImageView) findViewById(R.id.imageViewAllies1);
+        imAllies2 = (ImageView) findViewById(R.id.imageViewAllies2);
+        imAllies3 = (ImageView) findViewById(R.id.imageViewAllies3);
+        imAllies4 = (ImageView) findViewById(R.id.imageViewAllies4);
+        imAllies5 = (ImageView) findViewById(R.id.imageViewAllies5);
+
+        imEnemies1 = (ImageView) findViewById(R.id.imageViewEnemies1);
+        imEnemies2 = (ImageView) findViewById(R.id.imageViewEnemies2);
+        imEnemies3 = (ImageView) findViewById(R.id.imageViewEnemies3);
+        imEnemies4 = (ImageView) findViewById(R.id.imageViewEnemies4);
+        imEnemies5 = (ImageView) findViewById(R.id.imageViewEnemies5);
 
         imChoice1 = (ImageView) findViewById(R.id.imageViewChoice1);
         imChoice2 = (ImageView) findViewById(R.id.imageViewChoice2);
         imChoice3 = (ImageView) findViewById(R.id.imageViewChoice3);
 
+        //Test maj textview, TODO à finir
         tv = (TextView) findViewById(R.id.textViewAllies1);
 
+        //Ajout de toutes les images pour les picks
+        //Le nom dans heroNames doit correspondre au nom de la ressource
         for(String hero : heroNames) {
             ImageView i = new ImageView(this);
             int id = getResources().getIdentifier(hero, "drawable", getPackageName());
             i.setImageResource(id);
             i.setOnClickListener(imgClickListener);
-            i.setPadding(2,0,2,0);
-            i.setTag(hero);
+            i.setPadding(2,0,2,0); //TODO pas propre
+            i.setTag(hero); //pour identifier l'image plus facilement
 
             imLayout.addView(i);
         }
     }
 
+    //OnClickListener sur la liste complète pour savoir quel héros est choisi
+    //TODO ajouter le même listener sur les images "conseil" pour simplifier les picks ?
     View.OnClickListener imgClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -96,7 +108,8 @@ public class MainActivity extends Activity {
         }
     };
 
-
+    //Récupère les 3 meilleurs picks contre heroName et maj l'interface
+    //TODO factoriser
     private void makeJsonArrayRequest(final String heroName) {
 
         Log.d(TAG, "JsonArrayRequest with " + heroName);
@@ -110,6 +123,8 @@ public class MainActivity extends Activity {
                     String[] bestVss = {"","",""};
                     double bestWinrate = 100;
 
+                    //On parcours les résultats héros par héros pour avoir le meilleur winrate vs heroName
+                    //TODO pt-ê faut-il parcourir la boucle 3 fois pour que ce soit juste ?
                     for (int i = 0; i < response.length(); i++) {
                         jsonResponse = "";
 
@@ -127,6 +142,7 @@ public class MainActivity extends Activity {
 
                     }
 
+                    //Changement des noms avec caractères spéciaux pour que le nom corresponde aux ressources de l'app
                     for(int i = 0 ; i < bestVss.length ; i++) {
                         bestVss[i] = bestVss[i].equals("The Lost Vikings") ? "lostvikings" : bestVss[i];
                         bestVss[i] = bestVss[i].equals("Sgt. Hammer") ? "sgthammer" : bestVss[i];
@@ -136,19 +152,10 @@ public class MainActivity extends Activity {
                         bestVss[i] = bestVss[i].equals("Li-Ming") ? "liming" : bestVss[i];
                     }
 
-                    int idHero = getResources().getIdentifier(bestVss[0].toLowerCase(), "drawable", getPackageName());
-                    imChoice1.setTag(bestVss[0]);
-                    imChoice1.setImageResource(idHero);
+                    updateIuTop3(bestVss[0], bestVss[1], bestVss[2]);
 
-                    int idHero2 = getResources().getIdentifier(bestVss[1].toLowerCase(), "drawable", getPackageName());
-                    imChoice2.setTag(bestVss[1]);
-                    imChoice2.setImageResource(idHero2);
+                    Log.d(TAG, bestVss[0] + ", " + bestVss[1] + ", " + bestVss[2] + " best Vs " + heroName);
 
-                    int idHero3 = getResources().getIdentifier(bestVss[2].toLowerCase(), "drawable", getPackageName());
-                    imChoice3.setTag(bestVss[2]);
-                    imChoice3.setImageResource(idHero3);
-
-                    Log.d(TAG, bestVss[0] + "," + bestVss[1] + "," + bestVss[2] + " best Vs " + heroName);
                 } catch (JSONException e) {
                     e.printStackTrace();
                     Toast.makeText(getApplicationContext(),
@@ -171,5 +178,21 @@ public class MainActivity extends Activity {
         JsonArrayRequest req = new JsonArrayRequest(baseUrl + heroName + ".json", respListener, errListener);
 
         AppController.getInstance().addToRequestQueue(req);
+    }
+
+    //Maj de l'interface selon les meilleurs héros obtenus
+    public void updateIuTop3(String name1, String name2, String name3) {
+
+        int idHero = getResources().getIdentifier(name1.toLowerCase(), "drawable", getPackageName());
+        imChoice1.setTag(name1);
+        imChoice1.setImageResource(idHero);
+
+        int idHero2 = getResources().getIdentifier(name2.toLowerCase(), "drawable", getPackageName());
+        imChoice2.setTag(name2);
+        imChoice2.setImageResource(idHero2);
+
+        int idHero3 = getResources().getIdentifier(name3.toLowerCase(), "drawable", getPackageName());
+        imChoice3.setTag(name3);
+        imChoice3.setImageResource(idHero3);
     }
 }
