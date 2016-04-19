36be64474baa6459237617607b415352c2888e63
Salkyn <maxence.vialla@gmail.com>
2016-04-19 10:36:30 +0200

    switch teams + enlight next picks

--- app/src/main/java/com/kronologia/hotspicker/IU.java
+++ app/src/main/java/com/kronologia/hotspicker/IU.java
@@ -2,7 +2,15 @@ package com.kronologia.hotspicker;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.Display;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * Created by Maxence on 07/04/2016.
@@ -12,11 +20,13 @@ public class IU {
     private Resources res;
     private String packageName;
     private Activity act;
+    private String TAG;
 
     public IU(Resources r, String pack, Activity a) {
         this.res = r;
         this.packageName = pack;
         this.act = a;
+        this.TAG = IU.class.getSimpleName();
     }
 
     //Maj de l'interface selon les meilleurs héros obtenus
@@ -37,5 +47,64 @@ public class IU {
         int idHero3 = res.getIdentifier(name3.toLowerCase(), "drawable", packageName);
         imChoice3.setTag(name3);
         imChoice3.setImageResource(idHero3);
+    }
+
+    public void resetNextPicks(ImageView[] ivs) {
+
+        int idDrawable_Allies = res.getIdentifier("default1", "drawable", packageName);
+        Drawable allies = res.getDrawable(idDrawable_Allies);
+
+        int idDrawable_Ennemies = res.getIdentifier("default2", "drawable", packageName);
+        Drawable ennemies = res.getDrawable(idDrawable_Ennemies);
+
+        for(int i = 0 ; i < 10 ; i++) {
+            String tagParent = ((LinearLayout)ivs[i].getParent()).getTag().toString();
+
+            //Permet de savoir dans quel équipe on enlève un membre
+            if(tagParent.equals("allies")) {
+                ivs[i].setImageDrawable(allies);
+            } else {
+                ivs[i].setImageDrawable(ennemies);
+            }
+        }
+    }
+
+
+    public Drawable resize(Drawable image, int width) {
+
+        Bitmap b = ((BitmapDrawable)image).getBitmap();
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, width/10, width/10, false);
+        return new BitmapDrawable(res, bitmapResized);
+    }
+
+    public void lightNextPicks(ImageView[] ivs, int currentPickNumber) {
+        if (currentPickNumber == 10) {
+            return;
+        }
+
+        int idDrawable_toPickAllies = res.getIdentifier("default1_c", "drawable", packageName);
+        Drawable allies = res.getDrawable(idDrawable_toPickAllies);
+
+        int idDrawable_toPickEnnemies = res.getIdentifier("default2_c", "drawable", packageName);
+        Drawable ennemies = res.getDrawable(idDrawable_toPickEnnemies);
+
+        String tagCurrentParent = ((LinearLayout)ivs[currentPickNumber].getParent()).getTag().toString();
+
+        if(tagCurrentParent.equals("allies")) {
+            ivs[currentPickNumber].setImageDrawable(allies);
+        } else {
+            ivs[currentPickNumber].setImageDrawable(ennemies);
+        }
+
+        if(currentPickNumber != 9) {
+            String tagNextParent = ((LinearLayout)ivs[currentPickNumber+1].getParent()).getTag().toString();
+            if(tagCurrentParent.equals(tagNextParent)) {
+                if(tagCurrentParent.equals("allies")) {
+                    ivs[currentPickNumber+1].setImageDrawable(allies);
+                } else {
+                    ivs[currentPickNumber+1].setImageDrawable(ennemies);
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/kronologia/hotspicker/MainActivity.java
+++ app/src/main/java/com/kronologia/hotspicker/MainActivity.java
@@ -9,7 +9,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +21,7 @@ import java.util.Arrays;
 public class MainActivity extends Activity {
 
     public JSONRequests jsonRequests;
+    public IU iu;
 
     private static String TAG = MainActivity.class.getSimpleName();
 
@@ -31,7 +31,9 @@ public class MainActivity extends Activity {
     TextView tvAllies1, tvAllies2, tvAllies3, tvAllies4, tvAllies5;
     TextView tvennemies1, tvennemies2, tvennemies3, tvennemies4, tvennemies5;
 
-    Button resetButton;
+    Button resetButton, switchTeamsButton;
+
+    Drawable topickAllies_drawable, topickEnnemies_drawable;
 
     LinearLayout imLayout;
 
@@ -41,7 +43,8 @@ public class MainActivity extends Activity {
             "nazeebo","sgthammer","sylvanas","zagara","brightwing","kharazim",
             "lili","ltmorales","malfurion","rehgar","tassadar","tyrande","uther",
             "anubarak","artanis","arthas","chen","cho","diablo","etc","johanna",
-            "leoric","muradin","rexxar","sonya","stitches","tyrael","liming","xul","dehaka"};
+            "leoric","muradin","rexxar","sonya","stitches","tyrael","liming","xul","dehaka"/*,
+            "tracer"*/};
 
     int draftPickOrder = 0;
 
@@ -54,6 +57,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         jsonRequests = new JSONRequests(getResources(), getPackageName(), this);
+        iu = new IU(getResources(), getPackageName(), this);
 
         Arrays.sort(heroNames); //classement alphabétique des noms pour l'affichage
 
@@ -93,8 +97,21 @@ public class MainActivity extends Activity {
         resetButton = (Button) findViewById(R.id.newDraftButton);
         resetButton.setOnClickListener(newDraftListener);
 
-        imPickOrder = setTeamOrder(1); //La team "Allies" est la première à pick
-        tvPickOrder = setTvTeamOrder(1);
+        switchTeamsButton = (Button) findViewById(R.id.switchTeamsButton);
+        switchTeamsButton.setOnClickListener(switchTeamsListener);
+
+        int idDrawable_topickAllies = getResources().getIdentifier("default1_c", "drawable", getPackageName());
+        topickAllies_drawable = getResources().getDrawable(idDrawable_topickAllies);
+
+        int idDrawable_topickEnnemies = getResources().getIdentifier("default2_c", "drawable", getPackageName());
+        topickEnnemies_drawable = getResources().getDrawable(idDrawable_topickEnnemies);
+
+        setTeamOrder(1); //La team "Allies" est la première à pick par défaut
+        setTvTeamOrder(1);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
 
         //Ajout de toutes les images pour les picks
         //Le nom dans heroNames doit correspondre au nom de la ressource
@@ -102,7 +119,7 @@ public class MainActivity extends Activity {
             ImageView i = new ImageView(this);
             int id = getResources().getIdentifier(hero, "drawable", getPackageName());
             Drawable d = getResources().getDrawable(id);
-            i.setImageDrawable(resize(d));
+            i.setImageDrawable(iu.resize(d, size.x));
             // i.setImageResource(id);
 
             i.setOnClickListener(imgClickListener);
@@ -124,6 +141,13 @@ public class MainActivity extends Activity {
         @Override
         public void onClick(View v) {
 
+            if(draftPickOrder == 0) {
+                switchTeamsButton.setVisibility(View.GONE);
+                imChoice1.setVisibility(View.VISIBLE);
+                imChoice2.setVisibility(View.VISIBLE);
+                imChoice3.setVisibility(View.VISIBLE);
+            }
+
             if(draftPickOrder < 10) {
 
                 String n1 = v.getTag().toString();
@@ -143,6 +167,8 @@ public class MainActivity extends Activity {
                     addPick(n1);
                     updateTeams();
                 }
+
+                iu.lightNextPicks(imPickOrder, draftPickOrder);
 
                 if(draftPickOrder == 10) {
                     endDraft();
@@ -176,43 +202,29 @@ public class MainActivity extends Activity {
         return super.onKeyDown(keyCode, event);
     }
 
-    public ImageView[] setTeamOrder(int alliesTeamOrder) {
-
-        ImageView result[];
+    public void setTeamOrder(int alliesTeamOrder) {
 
         if(alliesTeamOrder==1) {
-            result = new ImageView[] {imAllies1,imEnnemies1,imEnnemies2,imAllies2,imAllies3,imEnnemies3,imEnnemies4,imAllies4,imAllies5,imEnnemies5};
+
+            imPickOrder = new ImageView[] {imAllies1,imEnnemies1,imEnnemies2,imAllies2,imAllies3,imEnnemies3,imEnnemies4,imAllies4,imAllies5,imEnnemies5};
+            imAllies1.setImageDrawable(topickAllies_drawable);
         } else {
-            result = new ImageView[] {imEnnemies1,imAllies1,imAllies2,imEnnemies2,imEnnemies3,imAllies3,imAllies4,imEnnemies4,imEnnemies5,imAllies5};
+            imPickOrder = new ImageView[] {imEnnemies1,imAllies1,imAllies2,imEnnemies2,imEnnemies3,imAllies3,imAllies4,imEnnemies4,imEnnemies5,imAllies5};
+            imEnnemies1.setImageDrawable(topickEnnemies_drawable);
         }
 
-        return result;
     }
 
-    public TextView[] setTvTeamOrder(int alliesTeamOrder) {
-
-        TextView result[];
+    public void setTvTeamOrder(int alliesTeamOrder) {
 
         if(alliesTeamOrder==1) {
-            result = new TextView[] {tvAllies1,tvennemies1,tvennemies2,tvAllies2,tvAllies3,tvennemies3,tvennemies4,tvAllies4,tvAllies5,tvennemies5};
+            tvPickOrder = new TextView[] {tvAllies1,tvennemies1,tvennemies2,tvAllies2,tvAllies3,tvennemies3,tvennemies4,tvAllies4,tvAllies5,tvennemies5};
         } else {
-            result = new TextView[] {tvennemies1,tvAllies1,tvAllies2,tvennemies2,tvennemies3,tvAllies3,tvAllies4,tvennemies4,tvennemies5,tvAllies5};
+            tvPickOrder = new TextView[] {tvennemies1,tvAllies1,tvAllies2,tvennemies2,tvennemies3,tvAllies3,tvAllies4,tvennemies4,tvennemies5,tvAllies5};
         }
 
-        return result;
     }
 
-    private Drawable resize(Drawable image) {
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-
-        Bitmap b = ((BitmapDrawable)image).getBitmap();
-        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, width/10, width/10, false);
-        return new BitmapDrawable(getResources(), bitmapResized);
-    }
 
     private void addPick(String heroName) {
         int idHero = getResources().getIdentifier(heroName, "drawable", getPackageName());
@@ -273,6 +285,23 @@ public class MainActivity extends Activity {
             Intent intent = getIntent();
             finish();
             startActivity(intent);
+        }
+    };
+
+    View.OnClickListener switchTeamsListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            iu.resetNextPicks(imPickOrder);
+
+            if(imPickOrder[0].equals(imAllies1)) {
+                setTeamOrder(0);
+                setTvTeamOrder(0);
+            } else {
+                setTeamOrder(1);
+                setTvTeamOrder(1);
+            }
+
         }
     };
 }
